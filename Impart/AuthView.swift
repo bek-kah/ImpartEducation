08f2399bc232ae104dcc9c1e2fe2d8f5c1f2be60
Ulugbek Kahramonov <bek.kahramonov@icloud.com>
2026-03-10 21:53:17 -0400

    Set up authentication

--- Impart/AuthView.swift
+++ Impart/AuthView.swift
@@ -4,6 +4,10 @@ struct SignInView: View {
     @State private var email: String = ""
     @State private var password: String = ""
     
+    @State private var status: (Bool, String?) = (true, nil)
+    
+    private var supabase = Supabase()
+    
     var body: some View {
         NavigationStack {
             ScrollView {
@@ -17,9 +21,22 @@ struct SignInView: View {
                         .textInputAutocapitalization(.never)
                     SecureField("Password", text: $password)
                         .textInputAutocapitalization(.never)
+                        .padding(.bottom)
+                    
+                    if !status.0 {
+                        Text(status.1!)
+                        .foregroundStyle(.red)
+                        .font(.footnote)
+                    }
                 }
                 .padding()
                 .textFieldStyle(.roundedBorder)
+                
+                Button("Sign in") {
+                    Task {
+                        status = await supabase.createUser(email: email, password: password)
+                    }
+                }
                 
                 NavigationLink {
                     HomeView()
@@ -46,3 +63,14 @@ struct SignInView: View {
 #Preview {
     SignInView()
 }
+
+
+
+
+var weakPassword: Text = Text("""
+                        Password should contain at least one character of each:
+                        • lowercase letter
+                        • uppercase letter
+                        • number
+                        • special character
+                        """)
---
+++ Impart/Supabase.swift
@@ -0,0 +1,8 @@
+//
+//  Supabase.swift
+//  Impart
+//
+//  Created by Ulugbek Kahramonov on 3/6/26.
+//
+
+import Foundation
